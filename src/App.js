3af9a65f9f3f67e9261a85ad0b8589d357eee4fb
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:35:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useForm} from  "react-hook-form";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function MyForm(){
+  const{
+    register,
+    handleSubmit,
+    reset,
+    formState:{errors,isDirty,isSubmitting,isValid},
+    watch,
+  }=useForm({
+    defaultValues:{
+      name:'',
+      email:''
+    },
+  });
+    
+   const onSubmit=(data)=>{
+    console.log('Form data Submitted:',data);
+
+    reset();// after submitting the form it will reset the form
+   };
+
+   const nameValue=watch('name');//watch the name values when you are entering into the name field 
+
+
+    return(
+      <div className="container">
+        <h2 style={{textAlign:"center"}}>React Hook Form Example</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            Name:
+            <input {...register('name',{required:'Name Is Required',minLength:{value:3,message:'Name Must Be Atleast 3 Characters Long'},})}/>
+            {errors.name && <p className="error-message">{errors.name.message}</p>}
+          </label>
+
+          <label>
+            Email:
+            <input {...register('email',{required:'Email Is Required',pattern:{value:/^\S+@\S+$/i,message:'Invalid Email Address'},})}/>
+            {errors.email&&<p className="error-message">{errors.email.message}</p>}
+          </label>
+
+          <button type="submit" disabled={!isDirty || isSubmitting || !isValid} >
+          Submit
+        </button>
+        </form>
+        <div className="form-state">
+          <h3>Form State</h3>
+          <p>isDirty:{isDirty ?'Yes':'No'}</p>
+          <p>isSubmitting:{isSubmitting ?'Yes':'No'}</p>
+          <p>IsValid:{isValid ?'Yes':'No'}</p>
+        </div>
+        <div className="watched-values">
+          <h3>Watched Values</h3>
+          <p>Name Value:{nameValue}</p>
+        </div>
+      </div>
+    )
+
+
 }
-
-export default App;
+export default MyForm;
